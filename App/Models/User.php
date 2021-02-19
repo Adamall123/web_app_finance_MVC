0efc8a19dc5c87f0a079d39cb5f8ad56a46a27cb
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-02-19 13:03:27 +0100

    add register without validation

--- App/Models/User.php
+++ App/Models/User.php
@@ -11,16 +11,38 @@ use PDO;
  */
 class User extends \Core\Model
 {
-
-    /**
-     * Get all the users as an associative array
-     *
-     * @return array
-     */
-    public static function getAll()
-    {
-        $db = static::getDB();
-        $stmt = $db->query('SELECT id, name FROM users');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+	
+	/**
+	 * Error messages
+	 *
+	 * @var array
+	 */
+	 public $errors = [];
+	
+   public function __construct($data)
+   {
+	   //convert array to object properties
+	   foreach($data as $key => $value){
+		   $this->$key = $value;
+		   //echo $key;
+	   };
+   }
+   public function save()
+   {
+		
+		$password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+		
+		$sql = 'INSERT INTO users (name, password, email)
+				VALUES(:name, :password, :email)';
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+		
+		$stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+		$stmt->bindValue(':password', $password_hash, PDO::PARAM_STR);
+		$stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+		
+		$stmt->execute();
+   }
+   
+  
 }
